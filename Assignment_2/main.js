a27ece9ada5b1fc6744ad918379b464271c34a40
Muhammad Owais Qureshi <owaisqureshi34667@gmail.com>
2024-03-08 00:54:32 +0500

    Assignmint 45

--- Assignment_2/main.js
+++ Assignment_2/main.js
@@ -1,4 +1,52 @@
 "use strict";
+// Task 1 
+// Installation complete
+// Task 2 Personal Message: Store a person’s name in a variable, and print a message to that person. Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”
+let person = 'Hello Owais';
+console.log(person, ',"would you like to learn some TypeScript today?”"');
+// Task 3 Lowercase, Uppercase, Titalcase
+let Name = 'owAiS';
+let tital = Name.slice(0, 1).toUpperCase();
+let low = Name.slice(1).toLowerCase();
+let con = tital + low;
+console.log(con);
+// lowercase and uppercase
+let Name2 = 'owais QUreSHi';
+console.log(Name2.toLowerCase());
+console.log(Name2.toUpperCase());
+// Task 4 Famous Quote:
+console.log('Albert Einstein once said, “A person who never made a mistake never tried anything new.”');
+// Task 5 Famous Quote 2:
+let person2 = 'Albert Einstein';
+let message = 'once said, “A person who never made a mistake never tried anything new.”';
+console.log(person2, message);
+// Task 6 Stripping Names: Store a person’s name, and include some whitespace characters at the beginning and end of the name. Make sure you use each character combination, "\t" and "\n", at least once. Print the name once, so the whitespace around the name is displayed. Then print the name after striping the white spaces.
+let person3 = '\tO  wai   s';
+console.log(person3);
+//Task 7,8 Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8. Be sure to enclose your operations in print statements to see the results.
+console.log(4 + 4);
+console.log(12 - 4);
+console.log(4 * 2);
+console.log(8 / 1);
+// Task 9 Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.
+let num = 18;
+console.log('My Favorite number is', num);
+// Task 10 Adding commints
+// Task 11 Names Arry: Store the names of a few of your friends in a array called names. Print each person’s name by accessing each element in the list, one at a time.
+let Friends = ['Farooq', 'Mudabbir', 'Hunain'];
+console.log(Friends[0]);
+console.log(Friends[1]);
+console.log(Friends[2]);
+// Task 12 Greetings Start with the array you used in Exercise 11, but instead of just printing each person’s name, print a message to them. The text of each message should be the same, but each message should be personalized with the person’s name.
+let friends = ['farooq', 'mudabbir', 'Anus'];
+console.log(friends[0], 'is my best friend');
+console.log(friends[1], 'is my best friend');
+console.log(friends[2], 'is my best friend');
+// Task 13 Your Own Array: Think of your favorite mode of transportation, such as a motorcycle or a car, and make a list that stores several examples. Use your list to print a series of statements about these items, such as “I would like to own a Honda motorcycle.
+let arr = ['Car', 'Bike', 'Bus'];
+console.log('I like Civic', arr[0]);
+console.log('I would like to own a Honda motor', arr[1]);
+console.log('Red', arr[0], 'arrived in karachi');
 // Task 14 Guest List:Guest List: If you could invite anyone, living or deceased, to dinner, who would you invite? Make a list that includes at least three people you’d like to invite to dinner. Then use your list to print a message to each person, inviting them to dinner.
 let Guest = ['Farooq', 'Mudabbir', 'Owais'];
 console.log('Salam', "", Guest[0], ' i invite tonight dinner with me');
